52ee4ea6d857cbc1bb71c075a370ea4fc65ed821
git user <git@testbox.(none)>
2013-12-28 13:47:28 +0530

    adding python lib files

---
+++ for.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+for letter in 'Python':     # First Example
+   print 'Current Letter :', letter
---
+++ input.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+# [x*5 for x in range(2,10,2)]
+str = input("Enter your input: ");
+print "Received input is : ", str
