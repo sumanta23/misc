eddb862a51ac8c7b92723d4e64c8093d04b65131
sumanta23 <nandisumanta0@gmail.com>
2014-01-21 23:53:06 +0530

    Creating Terminal components

--- src/org/sumanta/gui/panel/TerminalPanel.java
+++ src/org/sumanta/gui/panel/TerminalPanel.java
@@ -1,5 +1,19 @@
 package org.sumanta.gui.panel;
 
-public class TerminalPanel {
+public class TerminalPanel Panel {
 
+	public TerminalPanel() {
+		// TODO Auto-generated constructor stub
+		JLabel ta=new JLabel();
+		this.setLayout(null);
+        ta.setEnabled(false);
+        JScrollPane scroll = new JScrollPane (ta);
+    scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+          scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+          
+    scroll.setBounds(0,0,TabbedPane.tabbedPane.getSelectedComponent().getWidth(),TabbedPane.tabbedPane.getSelectedComponent().getHeight());
+    ta.setSize(400, 300);
+    this.add(scroll);
+    ta.setText("jk");
+	}
 }
