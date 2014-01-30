ec3c8ba19da927aaee1ae4baf3fc38949a8f03e2
git user <git@testbox.(none)>
2014-01-30 18:26:33 +0530

    after ssh inclution

---
+++ src/org/sumanta/util/IOUtils.java
@@ -0,0 +1,22 @@
+package org.sumanta.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class IOUtils {
+
+	public static String fromStream(InputStream in) throws IOException
+	{
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+	    StringBuilder out = new StringBuilder();
+	    String newLine = System.getProperty("line.separator");
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	        out.append(line);
+	        out.append(newLine);
+	    }
+	    return out.toString();
+	}
+}
